e470956b7def1e49dc5a96d0d8d344fca14c8d71
Soham Kane <skninja128@gmail.com>
2024-10-15 01:17:41 +0530

    Add files via upload

---
+++ Assignments_On_C/Operators/Comparison Operators/Comparison.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z,a,b,c;
+    x=15;
+    y=17;
+    z=20;
+    a = x; // Assign x to a
+    if(a<=y)
+    {
+        printf("A is Smaller %d",a); // Correct formatting
+        printf("\n");
+        return 0;
+    }
+}
