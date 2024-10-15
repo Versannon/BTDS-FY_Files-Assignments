a4f0e64783d364ad76bd7541fe8bb01acea13ae2
Soham Kane <skninja128@gmail.com>
2024-10-15 23:28:36 +0530

    All Comparison Operator Operations

--- Assignments_On_C/Operators/Comparison Operators/Comparison.C
+++ Assignments_On_C/Operators/Comparison Operators/Comparison.C
@@ -1,15 +1,49 @@
-#include<stdio.h>
-int main()
-{
-    int x,y,z,a,b,c;
-    x=15;
-    y=17;
-    z=20;
-    a = x; // Assign x to a
-    if(a<=y)
-    {
-        printf("A is Smaller %d",a); // Correct formatting
-        printf("\n");
-        return 0;
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    // Addition
+    int sum = x + y;
+    printf("Sum: %d\n", sum);
+
+    // Comparison Operators with if statements
+    if (x == y) {
+        printf("x is equal to y\n");
+    } else {
+        printf("x is not equal to y\n");
     }
+
+    if (x != y) {
+        printf("x is not equal to y\n");
+    } else {
+        printf("x is equal to y\n");
+    }
+
+    if (x > y) {
+        printf("x is greater than y\n");
+    } else {
+        printf("x is not greater than y\n");
+    }
+
+    if (x < y) {
+        printf("x is less than y\n");
+    } else {
+        printf("x is not less than y\n");
+    }
+
+    if (x >= y) {
+        printf("x is greater than or equal to y\n");
+    } else {
+        printf("x is not greater than or equal to y\n");
+    }
+
+    if (x <= y) {
+        printf("x is less than or equal to y\n");
+    } else {
+        printf("x is not less than or equal to y\n");
+    }
+
+    return 0;
 }
