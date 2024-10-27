800fc77d973fde5068d6da21f32102cbeef8a74d
Soham Kane <skninja128@gmail.com>
2024-10-27 23:25:06 +0530

    none

--- Assignments_On_C/Operators/Bit-Wise Operators/Bitwise_XOR.C
+++ Assignments_On_C/Operators/Bit-Wise Operators/Bitwise_XOR.C
@@ -10,7 +10,7 @@ int main()
     Here,
     1: binary for 10 is 1010.
     2: binary for 5 is 0101.
-    3: by XOR '^', a^b gives 1111
+    3: by XOR '^', x^y gives 1111
     4: binary for 1111 is 15
     */
     printf("x ^ y = %d\n", ans); 
