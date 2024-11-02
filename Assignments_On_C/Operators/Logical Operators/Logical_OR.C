87917adf3e3645586b06b39e54693f59b6ccd85b
Soham Kane <skninja128@gmail.com>
2024-11-02 21:50:46 +0530

    new file

--- Assignments_On_C/Operators/Logical Operators/Logical_OR.C
+++ Assignments_On_C/Operators/Logical Operators/Logical_OR.C
@@ -2,7 +2,15 @@
 /*Since Logical Operators work best in
 a "if else" statement
 I will use if else to show Logical OR type.*/
-int main()
+int main() 
 {
+    int x = 5;
+    int y = 0;
+    if (x == 0 || y == 0) {
+        printf("At least one of the numbers is equal to 0.\n");
+    } else {
+        printf("Neither of the numbers is equal to 0.\n");
+    }
+
     return 0;
 }
