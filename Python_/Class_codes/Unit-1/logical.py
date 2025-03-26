5a6f8c3749be2bb82ba52ee31465e1fcfbf8ea8b
Soham Kane <skninja128@gmail.com>
2025-03-26 14:24:33 +0530

    deletes

--- Python_/Class_codes/Unit-1/logical.py
+++
@@ -1,9 +0,0 @@
-num1=80
-num2=60
-num3=70
-if num1>num2 and num1>num3:
-    print(num1, "is greater")
-elif num2>num1 and num2>num3:
-    print(num2, "is greater")
-else:
-    print(num3, "is greater")
